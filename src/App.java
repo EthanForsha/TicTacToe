25bac8871d7aad612bf791b5d969173e934bfb04
EthanForsha <forshaer@jacks.sfasu.edu>
2022-10-07 13:13:28 -0500

    First Commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import cs331Helper.cs331TicTacToeFrame;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    public void start(Stage stage){
+
+        cs331TicTacToeFrame frame = new cs331TicTacToeFrame(stage);
+     
+        // Add the game here to the frame
+        
+        
+    }
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
